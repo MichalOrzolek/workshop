3d64f8d5bf21b6e5151dc0e98d8719c2dc9a5985
Blazej <blazej.laskowski.egs@gmail.com>
2026-02-24 12:30:41 +0100

    first admin/models approach - customer

--- codaro/crm/admin.py
+++ codaro/crm/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from .models import Country, LeadSource, Customer
 
-# Register your models here.
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+    ]
+    
+    search_fields = [
+        "name",
+    ]
+
+
+@admin.register(LeadSource)
+class LeadSourceAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+    ]
+    
+    search_fields = [
+        "name",
+    ]
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "email",
+        "country",
+        "lead_source",
+        "lead_owner",
+    ]
+    
+    search_fields = [
+        "name",
+        "email",
+        "country__name",
+        "lead_source__name",
+        "lead_owner__username",
+        "lead_owner__first_name",
+        "lead_owner__last_name",
+    ]
+
+    autocomplete_fields = (
+        "country",
+        "lead_source",
+        "lead_owner",
+    )
--- codaro/crm/models.py
+++ codaro/crm/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class LeadSource(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    lead_source = models.ForeignKey(LeadSource, on_delete=models.CASCADE, blank=True, null=True)
+    lead_owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    vat_number = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
